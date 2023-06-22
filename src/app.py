1a69a12dad3156b013f3310e2bdb9dddbcb4eb6f
MariaDiezEsteve <diezesteve@outlook.es>
2023-06-22 14:26:33 +0200

    incluida la parte de get all books

--- src/app.py
+++ src/app.py
@@ -1,37 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import os
+
 
 #Import the file database.py
 import database as db
 
-#1. ACCESS TO A FILE TO SEND A LOCALHOST
-#Create a variable where is the name of directory's application
-#We're going to use it a function's os (dirname) to access a absolute path
-#file: directory which we want to go into
 
-template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-
-#2. JOIN SRC AND TEMPLATES WITH FILE PROJECT
-template_dir = os.path.join(template_dir,'src', 'templates')
-
-#3. INITIALIZING FLASK TO EXECUTE THE APLICATION IN LOCALHOST
+#INITIALIZING FLASK TO EXECUTE THE APLICATION IN LOCALHOST
 #Declarate a variable to initializing with Flask
 #Template folder is the directory we are (index.html) is the same as template_dir
 #App will look for the template to execute into an scren
-app = Flask(__name__, template_folder = template_dir )
+app = Flask(__name__, template_folder = "templates" )
 
-#4. TO SET UP THE PRINCIPAL ROUTE
+#TO SET UP THE PRINCIPAL ROUTE
 
 #Application routes
-
+ 
 @app.route("/")
 def home():
     return render_template('index.html')
 # We want to show the template index.html so we need to import render template from flask
- 
+
 @app.route("/books")
-def books():
-    cursor = db.database.cursor()
+def index():
+    con = db.conectdb()
+    cursor = con.cursor()
     cursor.execute("SELECT * FROM books")
 
     mybooks = cursor.fetchall()
@@ -40,12 +33,12 @@ def books():
     for book in mybooks:
         books_array.append(dict(zip(books_col_Names, book)))
     cursor.close()
-    return render_template('table_books.html' , data = books_array )
-
-
+    return render_template('books.html', data = books_array)
+        
+    
 # TO EXECUTE THE APPLICATION
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)    
+   app.run(debug=True)
 #with app.run we're going to indicate that the app is going to be in development
 
 
--- src/database.py
+++ src/database.py
@@ -5,9 +5,44 @@ import mysql.connector
 
 #2. Set up to connect with database
 #create a variable to connect with mysql
-database = mysql.connector.connect(
-    host = "containers-us-west-59.railway.app",
-    user = "root",
-    password = "mzj8vF2N8BRe4h8m8cyB",
+# def conectdb():
+#     try:
+#         con = mysql.connector.connect(
+#             host = "containers-us-west-59.railway.app",
+#             user = "root",
+#             passwd = "mzj8vF2N8BRe4h8m8cyB",
+#             database = "railway",
+#             port = 5906 
+#         )
+#         print ("conectado ")
+#     except Exception as e:
+#         print (f"error de conexion {e}")
+
+#     return  con
+
+# conectdb()
+
+
+def conectdb():
+    host = "containers-us-west-59.railway.app"
+    user = "root"
+    passwd = "mzj8vF2N8BRe4h8m8cyB"
     database = "railway"
-)
+    port = 5906
+    auth_plugin='mysql_native_password'
+    
+    try:
+        con = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=passwd,
+            database=database,
+            port=port,
+            auth_plugin=auth_plugin
+        )
+        print("Conexión exitosa a la base de datos")
+        return con
+    except mysql.connector.Error as error:
+        print(f"Error al conectarse a la base de datos: {error}")
+        return None
+
